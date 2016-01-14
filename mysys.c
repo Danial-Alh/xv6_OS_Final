9dcbbca6cb71cd1e2b1f486e219b11eb7ef24408
dev-danial <d_alihosseiny@yahoo.com>
2016-01-14 10:39:20 +0330

    automated commit on 2016/01/14 10:39:20

--- mysys.c
+++ mysys.c
@@ -5,31 +5,59 @@
 //#include "user.h"
 
 
-//void
-//load(void)
-//{
-//    int fd;
-//    struct test t;
-//
-//    fd = open("backup", O_RDONLY);
-//    if(fd >= 0) {
-//        printf(1, "ok: read backup file succeed\n");
-//    } else {
-//        printf(1, "error: read backup file failed\n");
-//        exit();
-//    }
-//
-//    int size = sizeof(t);
-//    if(read(fd, &t, size) != size){
-//        printf(1, "error: read from backup file failed\n");
-//        exit();
-//    }
-//    printf(1, "file contents name %c and number %d", t.name, t.number);
-//    printf(1, "read ok\n");
-//    close(fd);
-//}
+#include "fcntl.h"
+
+void
+save(void)
+{
+    int fd;
+    struct test t;
+    t.name = 'd';
+    t.number = 25267;
+
+    fd = open("backup", O_CREATE | O_RDWR);
+    if(fd >= 0) {
+        printf(1, "ok: create backup file succeed\n");
+    } else {
+        printf(1, "error: create backup file failed\n");
+        exit();
+    }
+
+    int size = sizeof(t);
+    if(write(fd, &t, size) != size){
+        printf(1, "error: write to backup file failed\n");
+        exit();
+    }
+    printf(1, "write ok\n");
+    close(fd);
+}
+
+void
+load(void)
+{
+    int fd;
+    struct test t;
+
+    fd = open("backup", O_RDONLY);
+    if(fd >= 0) {
+        printf(1, "ok: read backup file succeed\n");
+    } else {
+        printf(1, "error: read backup file failed\n");
+        exit();
+    }
+
+    int size = sizeof(t);
+    if(read(fd, &t, size) != size){
+        printf(1, "error: read from backup file failed\n");
+        exit();
+    }
+    printf(1, "file contents name %c and number %d", t.name, t.number);
+    printf(1, "read ok\n");
+    close(fd);
+}
 
 #include "user.h"
+#include "proc.h"
 
 int main()
 {
@@ -38,6 +66,7 @@ int main()
 //    load();
 
 //    struct proc *proc1;
+    struct context
     copyproc(proc1);
     exit();
 
